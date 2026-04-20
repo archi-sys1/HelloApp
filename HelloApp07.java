22ca02ce71408d7ccdb53997100eba3c2b8c3e2d
archi-sys1 <mazumderarchisha@gmail.com>
2026-04-20 23:12:31 +0530

    Add files via upload

---
+++ HelloApp07.java
@@ -0,0 +1,11 @@
+public class HelloApp07{
+
+    public static void main(String[] args) {
+
+        String names = (args.length == 0)
+                ? "World"
+                : String.join(", ", args);
+
+        System.out.println("Hello, " + names + "!");
+    }
+}
